93300d9a2f8b6528ce08e2484604d6be37db1209
Maikel Arcia Carrazana <macarci@gmail.com>
2018-10-04 11:24:46 -0300

    Allowing custom names to collectionize

--- lib/cenit/multi_tenancy/scoped.rb
+++ lib/cenit/multi_tenancy/scoped.rb
@@ -4,10 +4,14 @@ module Cenit
       extend ActiveSupport::Concern
 
       included do
-        store_in collection: -> { Cenit::MultiTenancy.tenant_model.tenant_collection_name(to_s) }
+        store_in collection: -> { Cenit::MultiTenancy.tenant_model.tenant_collection_name(collectionizable_name) }
       end
 
       module ClassMethods
+
+        def collectionizable_name
+          to_s
+        end
 
         def mongoid_root_class
           @mongoid_root_class ||=
