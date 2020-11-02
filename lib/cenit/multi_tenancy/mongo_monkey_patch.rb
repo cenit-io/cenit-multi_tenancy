0c0864dc0faf4008242991c1b8160a539421c1df
Maikel Arcia Carrazana <macarci@gmail.com>
2020-11-02 07:49:21 -0300

    Update | Upgrading mongoid (pilot)

--- lib/cenit/multi_tenancy/mongo_monkey_patch.rb
+++ lib/cenit/multi_tenancy/mongo_monkey_patch.rb
@@ -1,68 +1,69 @@
-require 'mongo/database/view'
-require 'mongo/operation/commands/collections_info'
-require 'mongo/operation/commands/list_collections'
-
-module Mongo
-
-  class Database
-
-    class View
-
-      attr_reader :collections_filter
-
-      alias_method :mongo_collections_names, :collection_names
-
-      def collection_names(options = {})
-        @collections_filter = options.reject { |key, _| key == :batch_size }
-        mongo_collections_names
-      end
-
-      private
-
-      def collections_info_spec
-        {
-          selector: {
-            listCollections: 1,
-            cursor: batch_size ? { batchSize: batch_size } : {},
-            filter: collections_filter || {},
-          },
-          db_name: @database.name
-        }
-      end
-    end
-  end
-
-  module Operation
-
-    class CollectionsInfo
-
-      private
-
-      alias_method :mongo_selector, :selector
-
-      def selector
-        selector = mongo_selector
-        if (filter = spec[:selector]) && (filter = filter[:filter])
-          selector = { '$and' => [selector, filter] }
-        end
-        selector
-      end
-    end
-
-    class ListCollections
-
-      private
-
-      def selector
-        selector = spec[SELECTOR] || {}
-        selector[:listCollections] = 1
-        filter = { name: { '$not' => /system\.|\$/ } }
-        if selector.key?(:filter)
-          filter = { '$and' => [selector[:filter], filter] }
-        end
-        selector[:filter] = filter
-        selector
-      end
-    end
-  end
-end
+# TODO Remove this commented code when finished mongoDB upgrading
+# require 'mongo/database/view'
+# require 'mongo/operation/commands/collections_info'
+# require 'mongo/operation/commands/list_collections'
+#
+# module Mongo
+#
+#   class Database
+#
+#     class View
+#
+#       attr_reader :collections_filter
+#
+#       alias_method :mongo_collections_names, :collection_names
+#
+#       def collection_names(options = {})
+#         @collections_filter = options.reject { |key, _| key == :batch_size }
+#         mongo_collections_names
+#       end
+#
+#       private
+#
+#       def collections_info_spec
+#         {
+#           selector: {
+#             listCollections: 1,
+#             cursor: batch_size ? { batchSize: batch_size } : {},
+#             filter: collections_filter || {},
+#           },
+#           db_name: @database.name
+#         }
+#       end
+#     end
+#   end
+#
+#   module Operation
+#
+#     class CollectionsInfo
+#
+#       private
+#
+#       alias_method :mongo_selector, :selector
+#
+#       def selector
+#         selector = mongo_selector
+#         if (filter = spec[:selector]) && (filter = filter[:filter])
+#           selector = { '$and' => [selector, filter] }
+#         end
+#         selector
+#       end
+#     end
+#
+#     class ListCollections
+#
+#       private
+#
+#       def selector
+#         selector = spec[SELECTOR] || {}
+#         selector[:listCollections] = 1
+#         filter = { name: { '$not' => /system\.|\$/ } }
+#         if selector.key?(:filter)
+#           filter = { '$and' => [selector[:filter], filter] }
+#         end
+#         selector[:filter] = filter
+#         selector
+#       end
+#     end
+#   end
+# end
--- lib/cenit/multi_tenancy/scoped.rb
+++ lib/cenit/multi_tenancy/scoped.rb
@@ -21,42 +21,6 @@ module Cenit
               root
             end
         end
-
-        def with_current_tenant
-          with(Cenit::MultiTenancy.tenant_model.current_tenant)
-        end
-
-        def with(options)
-          tenant_option = false
-          tenant = nil
-          if options.is_a?(Cenit::MultiTenancy.tenant_model)
-            tenant_option = tenant = options
-            options = {}
-          elsif options.is_a?(Hash)
-            [
-              :tenant,
-              Cenit::MultiTenancy.tenant_model_key
-            ].each do |key|
-              next if tenant_option
-              (tenant_option = options.has_key?(key)) && (tenant = options.delete(key))
-            end
-          end
-          if block_given?
-            unless tenant.is_a?(Cenit::MultiTenancy.tenant_model)
-              tenant = Cenit::MultiTenancy.tenant_model.where(id: tenant).first
-            end
-            current = Cenit::MultiTenancy.tenant_model.current
-            Cenit::MultiTenancy.tenant_model.current = tenant
-            begin
-              yield(self)
-            ensure
-              Cenit::MultiTenancy.tenant_model.current = current
-            end
-          else
-            options = options.merge(collection: Cenit::MultiTenancy.tenant_model.tenant_collection_name(mongoid_root_class, tenant: tenant)) if tenant_option
-            super
-          end
-        end
       end
     end
   end
